b42b1dbce6a9884c69a1ff7762a995359bd91ba9
Austin <akomen04@gmail.com>
2023-03-21 01:09:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+const newHeaderText = document.createTextNode("Austin is the champion");
+newHeader.appendChild(newHeaderText);
+
